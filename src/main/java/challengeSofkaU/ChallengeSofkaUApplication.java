9d8009a3fc85611050268c100b315c63956056fa
Juan Pablo Velez <juan.velez993@gmail.com>
2022-03-17 15:36:38 -0500

    carpetas mvc creadas

---
+++ src/main/java/challengeSofkaU/ChallengeSofkaUApplication.java
@@ -0,0 +1,14 @@
+package challengeSofkaU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeSofkaUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeSofkaUApplication.class, args);
+
+	}
+
+}
